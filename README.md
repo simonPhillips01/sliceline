a3e783699c3a5185b89ee16a9385875b9f9d5d0f
Simon Phillips <simon.phillips01@icloud.com>
2019-07-29 18:02:49 +0100

    Added google fonts and global styled components

--- README.md
+++ README.md
@@ -66,3 +66,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+#Create global styled components
+https://www.styled-components.com/docs/api#createglobalstyle
--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
+    <>
+    <h1>SliceLine</h1>
+    <GlobalStyle/>
     <div>Hello SliceLine</div>
+    </>
   );
 }
 
