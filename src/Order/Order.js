20c325618be5ec3b4d6badd20736584f73af79cc
Simon Phillips <simon.phillips01@icloud.com>
2019-07-30 17:35:58 +0100

    Added list orders

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -20,13 +20,33 @@ const OrderContent = styled(DialogContent)`
     height: 100%;
 `
 
+const OrderContainer = styled.div`
+    padding: 10px 0px;
+    border-bottom: 1px solid #2d3436;
+`
+
+const OrderItem = styled.div`
+    padding: 10px 0px;
+`
+
 export function Order({orders}) {
     return <OrderStyled>
             {orders.length === 0 ? (
-            <OrderContent>
-                Your order's looking pretty empty.
-            </OrderContent> ) : (
-            <OrderContent> Found {orders.length} orders </OrderContent>)}
+            <OrderContent> Your order's looking pretty empty. </OrderContent>
+             ) : (
+            <OrderContent> 
+                {" "}
+                <OrderContainer> Your Order: </OrderContainer>
+                {" "}
+                {orders.map(order => (
+                    <OrderContainer>
+                        <OrderItem>
+                            {order.name}
+                        </OrderItem>
+                    </OrderContainer>
+                ))}
+            </OrderContent>
+            )}
             <DialogFooter>
                 <ConfirmButton>
                     Checkout
