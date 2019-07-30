cc5fc01a257b8df99309ed23b840a0cf77f7f396
Simon Phillips <simon.phillips01@icloud.com>
2019-07-30 12:14:21 +0100

    Cleanup app.js

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -20,7 +20,7 @@ const Logo = styled(Title)`
 export function Navbar() {
     return <NavBarStyled>
         <Logo>
-            Sliceline 🍕
+            Sliceline <span role="img" aria-label="pizza slice">🍕</span>
         </Logo>
     </NavBarStyled>
 }
